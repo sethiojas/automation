fd915d14106d5382593cbcc7da3fa7fb9ee97d04
Ojas Sethi <ojas.sethi@outlook.com>
2019-07-13 18:00:07 +0530

    for yoda quote feature

---
+++ yoda.py
@@ -0,0 +1,30 @@
+from random import choice
+def say_quote():
+    quotes = [
+    'Patience you must have, my young padawan.',
+    'Less of a shit give I could Not',
+    'Out of hand this shit is getting',
+    'The stupid is strong with this one',
+    'Keep moving I must, Wearing my fitbit I am',
+    'Pay attention in english class I do not, Speak my own way i will',
+    'Coffee i need, Or kill you i will',
+    'Offended you are, A shit i don\'t give',
+    'Your head, Hit it i will',
+    'Always pass the puff you must',
+    'Idiot you are. Breed you should not',
+    'Rollin\' they see me, Hatin\' they be',
+    'friday it is, Get drunk we must',
+    'Hold my farts in i will not',
+    'Pooped in your shoes i have',
+    'If onto your EX\'s Instagram you go, Only pain you will find',
+    'Much butt-hurt i sense in you, Cry like a baby you should',
+    'At me bro You come',
+    'Pissed me off you have',
+    'Please bitch, To the hand you talk, Listening I am not',
+    'Brings the boys to the yard my milkshake does',
+    'if stupid you are then speak you should not'
+    ]
+    return choice(quotes)
+
+if __name__ == '__main__':
+    main()

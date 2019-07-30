baf193f4543b24c1de6f4679f0dec1f03244150c
Ojas Sethi <sethi.ojas@gmail.com>
2019-07-30 12:47:32 +0530

    Code refactor
    Added a call to decrypt and parse function of functions.py file to get
    necessary data.
    No need to hardcode bot_id, bot_password and similar variable to automate.py
    Removed extra print statement which printed 'Sleeping...'
    Edited some comments.

--- automate.py
+++ automate.py
@@ -3,28 +3,11 @@ from os import system
 from mail_func import Mail
 import functions
 
-#-----------------------------TO BE FILLED--------------------------------------------------
-
-#bot id and password
-#gmail id required
-#if any other id is to be used then change the search function of IMAP
-bot_id = ''
-bot_passwd = ''
-
-#Mail id to receive email alerts on
-receiver_id = ''
-
-#instructions sent from only this email will be executed
-allowed_email = []
-sender_name = '' #Name of account Holder of the above give allowed_id
-
-#attachments (.txt files) in email get downloaded to this directory
-#eg /home/manjaro/Downloads/email 
-download_path = ''
-
-mail = Mail(bot_id, bot_passwd, receiver_id, allowed_email, sender_name, download_path)
-
-#--------------------------------------------------------------------------------------------
+data = functions.decrypt_and_parse_data()
+mail = Mail(
+	data['bot_id'], data['bot_passwd'], data['receiver_id'],
+	data['allowed_email'], data['sender_name'], data['download_path']
+	)
 
 try:
 	while True:
@@ -69,7 +52,6 @@ try:
 
 		sleep(5)
 		system('clear')
-		print('Sleeping....')
 		
 		#sleep for specified amount of time (Defaults to 5 minutes)
 		#before checking email again
